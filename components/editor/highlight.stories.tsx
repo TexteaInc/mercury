a4c49c940556ba65507c1ab0d5984312f0192575
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2025-06-27 13:33:39 +0800

    fix: story score

--- components/editor/highlight.stories.tsx
+++ components/editor/highlight.stories.tsx
@@ -24,12 +24,12 @@ export const Primary: Story = {
   args: {
     text: "Lorem ipsum odor amet, consectetuer adipiscing elit. Vitae auctor condimentum magnis massa bibendum imperdiet pellentesque. Ullamcorper sodales gravida blandit tellus; ornare leo fusce erat nisi. Bibendum ut dignissim amet neque eget cursus. Morbi ornare pharetra vel dapibus metus; mollis imperdiet nullam. Hac consectetur velit pretium turpis enim rutrum lorem.",
     highlights: [
-      { start: 0, end: 10, color: "hsl(210, 70%, 50%)" },
-      { start: 15, end: 25, color: "hsl(120, 40%, 60%)" },
-      { start: 20, end: 30, color: "hsl(240, 40%, 60%)" },
-      { start: 35, end: 50, color: "hsl(300, 40%, 60%)" },
-      { start: 40, end: 55, color: "hsl(30, 40%, 60%)" },
-      { start: 45, end: 60, color: "hsl(210, 70%, 50%)" },
+      { start: 0, end: 10, color: "hsl(210, 70%, 50%)", score: 0.9 },
+      { start: 15, end: 25, color: "hsl(120, 40%, 60%)", score: 0.8 },
+      { start: 20, end: 30, color: "hsl(240, 40%, 60%)", score: 0.7 },
+      { start: 35, end: 50, color: "hsl(300, 40%, 60%)", score: 0.6 },
+      { start: 40, end: 55, color: "hsl(30, 40%, 60%)", score: 0.5 },
+      { start: 45, end: 60, color: "hsl(210, 70%, 50%)", score: 0.4 },
     ],
   },
 }
